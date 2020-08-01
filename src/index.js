bc25ef4899b87793bfc987f4fd642e177a2cb06c
girishsontakke <girishsontakke7@gmail.com>
2020-08-01 12:25:46 +0530

    add bootstrap styling and components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';  
+import Counter from './components/counter';
 
+ReactDOM.render(<Counter/> , document.querySelector("#root"));
 
-const element = <h1> Hello World </h1>;
-
-ReactDOM.render(element, document.querySelector("#root"));
+serviceWorker.unregister();
