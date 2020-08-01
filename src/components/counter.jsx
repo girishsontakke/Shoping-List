62bb0e7e3f1cc640580daf9e4d11e3c0b504eee7
girishsontakke <girishsontakke7@gmail.com>
2020-08-01 12:26:29 +0530

    create first counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1> Hello World </h1>
+        <button> Increment </button>
+      </Fragment>
+    );
+  }
+}
+
+export default Counter;
