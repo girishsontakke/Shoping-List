441d03a179cb19374bf8894ef4d375ee2a5141ed
girishsontakke <girishsontakke7@gmail.com>
2020-08-01 20:46:49 +0530

    render dynamic list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,16 @@ import React, { Component, Fragment } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
   };
-
+  
   render() {
     return (
       <Fragment>
         <span className={`badge m-2 badge-${this.state.count===0? `warning`:`primary`}`}> {this.formatCount()}</span>
+        <ul>
+          {this.state.tags.map((tag, i)=> <li key={i}>{tag}</li>)}
+        </ul>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </Fragment>
     );
